0b5a6fa86da7dfb56a4bc74530669462bca24347
PabloFLanmarion <pabloflanmarion@gmail.com>
2020-05-22 23:56:15 -0300

    prepare project

---
+++ src/main/java/com/igreja/mapaassistencia/EscalaFlores1Application.java
@@ -0,0 +1,13 @@
+package com.igreja.mapaassistencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscalaFlores1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscalaFlores1Application.class, args);
+	}
+
+}
